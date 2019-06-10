6b06893f4f844b52e37971a22f44cb9fa3fd379a
Harold <hllauca@gmail.com>
2019-06-10 14:58:41 -0500

    Add existing file

--- R/Run_GR2M_SemiDistr.R
+++ R/Run_GR2M_SemiDistr.R
@@ -95,7 +95,7 @@ Run_GR2M_SemiDistr <- function(Parameters, Region, Location, FlowDir='Flow_Direc
     Factor     <- list()
     Inputs     <- list()
     FixInputs  <- list()
-    qMask      <- as.matrix(raster(path.mask))
+    qMask      <- raster(path.mask)
     # qRaster    <- qMask
     # qArray     <- array(NA, dim=c(nrow(qMask), ncol(qMask), time))
     # qBrick     <- brick(nr=nrow(qMask), nc=ncol(qMask), nl=time)
