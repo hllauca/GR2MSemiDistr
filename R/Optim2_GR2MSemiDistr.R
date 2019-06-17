8e67ec03fafa0559921c07cf0e84b70aba1951d7
Harold <hllauca@gmail.com>
2019-06-17 12:15:11 -0500

    Add existing file

--- R/Optim2_GR2MSemiDistr.R
+++ R/Optim2_GR2MSemiDistr.R
@@ -175,7 +175,7 @@ Optim2_GR2MSemiDistr <- function(Parameters, Parameters.Min, Parameters.Max, Opt
           # Evaluation criteria dataframe
             optim.df <- data.frame(KGE=round(KGE(Qsim, Qobs, na.rm=T), 3),
                                    NSE=round(NSE(Qsim, Qobs, na.rm=T), 3),
-                                   lnNSE=round(NSE(ln(Qsim), ln(Qobs), na.rm=T), 3),
+                                   lnNSE=round(NSE(log(Qsim), log(Qobs), na.rm=T), 3),
                                    RMSE=1-round(rmse(Qsim, Qobs, na.rm=T), 3),
                                    R=round(rPearson(Qsim, Qobs, na.rm=T), 3),
                                    PBIAS=round(pbias(Qsim, Qobs, na.rm=T), 3))
