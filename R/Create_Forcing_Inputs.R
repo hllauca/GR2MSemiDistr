4456d6305db863c6c99a123632a2242672c0c815
Harold <hllauca@gmail.com>
2019-12-13 20:21:45 -0500

    Add existing file

--- R/Create_Forcing_Inputs.R
+++ R/Create_Forcing_Inputs.R
@@ -10,7 +10,9 @@
 #' @param DateEnd Final date 'mm/yyyy' for export data.
 #' @param Factor Factor between 1 and 1.2 to buffer subbasins and extract data. 1 as default
 #' @param Positions Cell numbers to extract data faster for each subbasin. NULL as default
-#' @param all Conditional to consider all the period of the netCDF file. FALSE as default
+#' @param All Conditional to consider all the period of the netCDF file. FALSE as default
+#' @param Members Number of ensemble members. NULL as default.
+#' @param Horiz Number of months for forcastting. NULL as default.
 #' @return Export and save a text file with forcing data inputs (Dates, Precip, Evap, Qobs).
 #' @export
 #' @import  rgdal
@@ -20,7 +22,7 @@
 #' @import  ncdf4
 #' @import  parallel
 Create_Forcing_Inputs <- function(Shapefile, Database, Precip, PotEvap, Qobs=NULL, DateIni, DateEnd,
-                                  Resolution=0.01, Factor=1, Positions=NULL, all=FALSE){
+                                  Resolution=0.01, Factor=1, Positions=NULL, All=FALSE, Members=NULL, Horiz=NULL){
 
 # Shapefile=File.Shape
 # Database=Database
@@ -70,12 +72,12 @@ Create_Forcing_Inputs <- function(Shapefile, Database, Precip, PotEvap, Qobs=NUL
     # Create a vector of dates
       Ini <- paste0('01/',DateIni)
       End <- paste0('01/',DateEnd)
-      if(all==FALSE){
+      if(All==FALSE){
         DatesMonths <- seq(as.Date(Ini, "%d/%m/%Y"), as.Date(End, "%d/%m/%Y"), by='month')
       } else{
         DatesMonths <- seq(as.Date(Ini, "%d/%m/%Y"), as.Date(End, "%d/%m/%Y"), by='month')
         NDates      <- length(DatesMonths)
-        DatesMonths <- c(DatesMonths,rep(DatesMonths[c(NDates-2, NDates-1, NDates)], length=3*39))
+        DatesMonths <- c(DatesMonths,rep(DatesMonths[c(NDates-2, NDates-1, NDates)], length=Horiz*(Members-1)))
       }
 
 
@@ -121,7 +123,7 @@ Create_Forcing_Inputs <- function(Shapefile, Database, Precip, PotEvap, Qobs=NUL
                  })
       # stopCluster(cl) #Close the cluster
       mean.pp <- do.call(rbind, mean.pp)
-      if(all==FALSE){
+      if(All==FALSE){
         mean.pp <- round(mean.pp[1:length(DatesMonths),],1)
       } else{
         mean.pp <- round(mean.pp,1)
@@ -166,7 +168,7 @@ Create_Forcing_Inputs <- function(Shapefile, Database, Precip, PotEvap, Qobs=NUL
       })
       stopCluster(cl) #Close the cluster
       mean.pet <- do.call(rbind, mean.pet)
-      if(all==FALSE){
+      if(All==FALSE){
         mean.pet <- round(mean.pet[1:length(DatesMonths),],1)
       } else{
         mean.pet <- round(mean.pet,1)
