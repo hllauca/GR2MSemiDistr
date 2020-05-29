b510223cd1bf3a7e44b30ed84d0fcf104a62fe14
Harold <hllauca@gmail.com>
2020-05-29 12:14:03 -0500

    Add existing file

--- R/Create_Forcing_Inputs.R
+++ R/Create_Forcing_Inputs.R
@@ -193,7 +193,7 @@ Create_Forcing_Inputs <- function(Shapefile, Database, Precip, PotEvap, Qobs=NUL
     # Saving positions
     if(is.null(Positions)==TRUE){
       Positions <- list(PP=positionPP, PET=positionPET)
-      save(Positions, file=file.path(getwd(),'Positions_Forcing.Rda'))
+      save(Positions, file=file.path(getwd(),'Inputs','Positions_Forcing.Rda'))
     }
 
     # End
