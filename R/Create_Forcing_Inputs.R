e6da8d7f8d2c716a1bbe532ed2dafff22448280d
Harold Llauca <hllauca@gmail.com>
2025-08-04 10:27:38 -0500

    Add existing file

--- R/Create_Forcing_Inputs.R
+++ R/Create_Forcing_Inputs.R
@@ -1,162 +1,186 @@
-#' Extract and prepare model's inputs data in the airGR format (DatesR, P and E) from gridded P and E monthly data.
-#' @param Subbasins Subbasins' shapefile. Must contain the following attributes: 'Area' (in km2), 'Region' (in letters), and 'COMID' (identifier number).
-#' @param Precip  Raster brick of the precipitation data in [mm/month].
-#' @param PotEvap Raster brick of the evapotranspiration data in [mm/month].
-#' @param Qobs Observed streamflow data in [m3/s] at the basin outlet. Must have the dates of the output dataset.
-#' @param DateIni Initial date of the output database in 'mm/yyyy' format.
-#' @param DateEnd Ending date of the output database in 'mm/yyyy' format.
-#' @param IniGrids  Initial date of the gridded data (P and PE) in 'mm/yyy' format.
-#' @param Save   Boolean to save results as a text file in the 'Outputs' location. FALSE as default.
-#' @param Update Boolean for the updating mode where only the last month's values will be returned. FALSE as default.
-#' @param Members Número de miembros del conjunto modelo. Only for streamflow forecasting purposes. NULL por defecto.
-#' @return Return a dataframe of model's inputs data in the airGR format (DatesR, P, E, Q).
-#' @references Cesar Aybar, Carlos Fernández, Adrian Huerta, Waldo Lavado, Fiorella Vega & Oscar Felipe-Obando (2020) Construction of a high-resolution gridded rainfall dataset for Peru from 1981 to the present day, Hydrological Sciences Journal, 65:5, 770-785, DOI: 10.1080/02626667.2019.1649411
-#' @references Llauca H, Lavado-Casimiro W, Montesinos C, Santini W, Rau P. PISCO_HyM_GR2M: A Model of Monthly Water Balance in Peru (1981–2020). Water. 2021; 13(8):1048. https://doi.org/10.3390/w13081048
-#' @export
-#' @examples
-#' # Load data
-#' library(GR2MSemiDistr)
-#' data(pisco_pr)
-#' data(pisco_pe)
-#' data(qobs)
-#' data(roi)
+#' Extract and prepare model input data in the airGR format (DatesR, P, E, and optionally Q) using gridded monthly precipitation and evapotranspiration.
 #'
-#' # Create a database with model's inputs data
-#' data <- Create_Forcing_Inputs(Subbasins=roi,
-#'                               Precip=pisco_pr,
-#'                               PotEvap=pisco_pe,
-#'                               Qobs=qobs,
-#'                               DateIni='01/1981',
-#'                               DateEnd='12/2016',
-#'                               IniGrids='01/1981')
-#' View(data)
+#' This function extracts spatially averaged monthly precipitation and potential evapotranspiration values for each subbasin,
+#' and optionally includes observed streamflow data, to produce a formatted data frame compatible with the airGR hydrological modeling framework.
+#'
+#' @param Subbasins A `SpatVector` object containing the subbasins' geometries. Must include the following attributes: `'COMID'` (unique identifier), `'Area'` (in km²), and `'Region'` (string code for the hydro-climatic region).
+#' @param Precip A `SpatRaster` object with monthly precipitation data in [mm/month].
+#' @param PotEvap A `SpatRaster` object with monthly potential evapotranspiration data in [mm/month].
+#' @param Qobs Optional. A vector with observed streamflow in [m³/s] at the basin outlet.
+#' @param DateIni Start date of the output time series in `'mm/yyyy'` format.
+#' @param DateEnd End date of the output time series in `'mm/yyyy'` format.
+#' @param IniGrids Initial date of the gridded precipitation and evapotranspiration datasets in `'mm/yyyy'` format.
+#' @param Save Logical. If TRUE, saves the resulting data frame to a `.txt` file in the `'./Inputs'` directory. Default is FALSE.
+#' @param Update Logical. If TRUE, returns only the last month of data (useful for operational updates). Default is FALSE.
+#' @param Members Optional. Integer indicating the number of ensemble members for streamflow forecasting. If provided, the date vector will be repeated accordingly. Default is NULL.
+#'
+#' @return A data frame containing the model inputs in the airGR format, with columns:
+#' \describe{
+#'   \item{DatesR}{Monthly dates}
+#'   \item{P_x}{Mean precipitation for subbasin x, where x is the COMID}
+#'   \item{E_x}{Mean potential evapotranspiration for subbasin x, where x is the COMID}
+#'   \item{Q}{Observed streamflow [m³/s], if provided}
+#' }
+#'
+#' @references
+#' Aybar, C., Fernández, C., Huerta, A., Lavado, W., Vega, F., & Felipe-Obando, O. (2020). Construction of a high-resolution gridded rainfall dataset for Peru from 1981 to the present day. *Hydrological Sciences Journal*, 65(5), 770–785. https://doi.org/10.1080/02626667.2019.1649411
+#'
+#' Llauca, H., Lavado-Casimiro, W., Montesinos, C., Santini, W., & Rau, P. (2021). PISCO_HyM_GR2M: A Model of Monthly Water Balance in Peru (1981–2020). *Water*, 13(8), 1048. https://doi.org/10.3390/w13081048
+#'
+#' @export
+#'
+#' @examples
+#' library(GR2MSemiDistr)
+#' data(pisco_pr)  # Monthly precipitation SpatRaster
+#' data(pisco_pe)  # Monthly potential evapotranspiration SpatRaster
+#' data(qobs)      # Data frame with observed streamflow
+#' data(roi)       # Subbasins SpatVector
+#'
+#' # Create model input data
+#' data_inputs <- Create_Forcing_Inputs(
+#'   Subbasins = roi,
+#'   Precip = pisco_pr,
+#'   PotEvap = pisco_pe,
+#'   Qobs = qobs,
+#'   DateIni = '01/1981',
+#'   DateEnd = '12/2016',
+#'   IniGrids = '01/1981'
+#' )
+#' View(data_inputs)
+#'
 #' @import exactextractr
 #' @import terra
+#' @import sf
 #' @import lubridate
 #' @import tictoc
+
 Create_Forcing_Inputs <- function(Subbasins,
                                   Precip,
                                   PotEvap,
-                                  Qobs=NULL,
+                                  Qobs = NULL,
                                   DateIni,
                                   DateEnd,
-                                  IniGrids='01/1981',
-                                  Save=FALSE,
-                                  Update=FALSE,
-                                  Members=NULL){
+                                  IniGrids = '01/1981',
+                                  Save = FALSE,
+                                  Update = FALSE,
+                                  Members = NULL) {
 
-  # Load required packages
-  library(exactextractr)
-  library(terra)
-  library(lubridate)
-  library(tictoc)
   tic()
 
-  # Load subbasin data
-  roi   <- Subbasins
-  comid <- as.vector(roi$COMID)
+  # ==== Input validation ====
+  if (!inherits(Subbasins, "SpatVector")) {
+    stop("Argument 'Subbasins' must be of class 'SpatVector'.")
+  }
+  if (!"COMID" %in% names(Subbasins)) {
+    stop("The 'Subbasins' object must contain a field named 'COMID'.")
+  }
+  if (!is.null(Precip) && !inherits(Precip, "SpatRaster")) {
+    stop("Argument 'Precip' must be of class 'SpatRaster'.")
+  }
+  if (!is.null(PotEvap) && !inherits(PotEvap, "SpatRaster")) {
+    stop("Argument 'PotEvap' must be of class 'SpatRaster'.")
+  }
+
+  # ==== Format validation ====
+  date_format_check <- function(date_string) {
+    grepl("^\\d{2}/\\d{4}$", date_string)
+  }
+  if (!date_format_check(IniGrids)) {
+    stop("Argument 'IniGrids' must be in 'mm/yyyy' format (e.g., '01/1981').")
+  }
+  if (!date_format_check(DateIni)) {
+    stop("Argument 'DateIni' must be in 'mm/yyyy' format (e.g., '01/1990').")
+  }
+  if (!date_format_check(DateEnd)) {
+    stop("Argument 'DateEnd' must be in 'mm/yyyy' format (e.g., '12/2020').")
+  }
+
+  # ==== Extract subbasin IDs ====
+  comid <- as.vector(Subbasins$COMID)
   nsub  <- length(comid)
 
-  # Extract monthly precipitation data for each subbasin
-  if(is.null(Precip)==FALSE){
+  # ==== Helper function: mean areal extraction ====
+  extract_variable <- function(raster, variable_name) {
     cat('\f')
-    message('Calculating monthly mean-areal precipitation [mm/month]')
-    message('Please wait...')
-    pr <- Precip
-    if(Update==TRUE){
-      pr_mean <- round(t(exact_extract(pr, roi, 'mean', progress=FALSE)),1)[nlyr(pr),]
-      pr_mean <- as.data.frame(t(pr_mean))
-    }else{
-      if(Members==1 | is.null(Members)==TRUE){
-          dates <- seq(from=as.Date(paste0('01/',IniGrids), '%d/%m/%Y'),
-                       to=as.Date(paste0('01/',IniGrids), '%d/%m/%Y') + months(nlyr(pr)-1),
-                       by='month')
-          Ini   <- as.Date(paste0('01/',DateIni),'%d/%m/%Y')
-          End   <- as.Date(paste0('01/',DateEnd),'%d/%m/%Y')
-          ind   <- which(dates==Ini):which(dates==End)
-          dates <- dates[ind]
-          pr    <- pr[[ind]]
-        }
-      pr_mean <- round(t(exact_extract(pr, roi, 'mean', progress=FALSE)),1)
-    }
-  }
+    cat(paste0("Computing monthly ", variable_name, " for each subbasin\n"))
+    cat("Please wait...\n")
 
+    # Convert SpatVector to sf for exactextractr
+    Subbasins_sf <- sf::st_as_sf(Subbasins)
 
-  # Extract monthly mean-areal evapotranspiration
-  if(is.null(PotEvap)==FALSE){
-    cat('\f')
-    message('Calcutaling monthly mean-areal evapotranspiration [mm/month]')
-    message('Please wait...')
-    pe <- PotEvap
-    if(Update==TRUE){
-      pe_mean <- round(t(exact_extract(pe, roi, 'mean', progress=FALSE)),1)[nlyr(pe),]
-      pe_mean <- as.data.frame(t(pe_mean))
-    }else{
-      if(Members==1 | is.null(Members)==TRUE){
-        dates <- seq(from=as.Date(paste0('01/',IniGrids), '%d/%m/%Y'),
-                     to=as.Date(paste0('01/',IniGrids), '%d/%m/%Y') + months(nlyr(pe)-1),
-                     by='month')
-        Ini   <- as.Date(paste0('01/',DateIni),'%d/%m/%Y')
-        End   <- as.Date(paste0('01/',DateEnd),'%d/%m/%Y')
-        ind   <- which(dates==Ini):which(dates==End)
-        dates <- dates[ind]
-        pe    <- pe[[ind]]
+    if (is.null(Members)) {
+      all_dates <- seq(
+        from = as.Date(paste0("01/", IniGrids), "%d/%m/%Y"),
+        to   = as.Date(paste0("01/", IniGrids), "%d/%m/%Y") + months(nlyr(raster) - 1),
+        by   = "month"
+      )
+      Ini  <- as.Date(paste0("01/", DateIni), "%d/%m/%Y")
+      End  <- as.Date(paste0("01/", DateEnd), "%d/%m/%Y")
+      ind  <- which(all_dates >= Ini & all_dates <= End)
+
+      extracted <- t(exact_extract(raster[[ind]], Subbasins_sf, "mean", progress = FALSE))
+
+      if (Update) {
+        extracted <- as.data.frame(extracted[nrow(extracted), , drop = FALSE])
       }
-      pe_mean <- round(t(exact_extract(pe, roi, 'mean', progress=FALSE)),1)
+    } else {
+      extracted <- t(exact_extract(raster, Subbasins_sf, "mean", progress = FALSE))
+    }
+
+    return(extracted)
+  }
+
+  # ==== Extract data ====
+  if (!is.null(Precip)) {
+    Prec <- extract_variable(Precip, "precipitation")
+  }
+
+  if (!is.null(PotEvap)) {
+    Evap <- extract_variable(PotEvap, "evapotranspiration")
+  }
+
+  # ==== Build date vector ====
+  if (Update) {
+    DatesMonths <- as.Date(paste0("01/", DateEnd), "%d/%m/%Y")
+  } else {
+    Ini <- as.Date(paste0("01/", DateIni), "%d/%m/%Y")
+    End <- as.Date(paste0("01/", DateEnd), "%d/%m/%Y")
+    DatesMonths <- seq(Ini, End, by = "month")
+    if (!is.null(Members)) {
+      DatesMonths <- rep(DatesMonths, times = Members)
     }
   }
 
+  # ==== Assemble output dataframe ====
+  data_list <- list(DatesR = DatesMonths)
 
-  # Create a vector of dates
-  if(Update==TRUE){
-    DatesMonths <- as.Date(paste0('01/',DateEnd), "%d/%m/%Y")
-  }else{
-    Ini <- paste0('01/',DateIni)
-    End <- paste0('01/',DateEnd)
-    DatesMonths <- seq(as.Date(Ini, "%d/%m/%Y"),
-                       as.Date(End, "%d/%m/%Y"),
-                       by='month')
-    if(is.null(Members)==FALSE){
-      DatesMonths <- rep(DatesMonths, times=Members)
-    }
+  if (!is.null(Precip)) {
+    Prec <- round(Prec, 1)
+    colnames(Prec) <- paste0("P_", comid)
+    data_list <- c(data_list, as.data.frame(Prec))
   }
 
+  if (!is.null(PotEvap)) {
+    Evap <- round(Evap, 1)
+    colnames(Evap) <- paste0("E_", comid)
+    data_list <- c(data_list, as.data.frame(Evap))
+  }
 
-  # Prepare database in airGR format
-    if(is.null(Qobs)==FALSE & is.null(Precip)==FALSE & is.null(PotEvap)==FALSE){
-      qm_obs        <- Qobs[,2]
-      Ans           <- data.frame(DatesMonths, round(pr_mean,1), round(pe_mean,1), round(qm_obs,3))
-      colnames(Ans) <- c('DatesR', paste0('P_',comid), paste0('E_',comid), 'Q')
-    }
-    if(is.null(Qobs)==FALSE & is.null(Precip)==FALSE & is.null(PotEvap)==TRUE){
-      qm_obs        <- Qobs[,2]
-      Ans           <- data.frame(DatesMonths, round(pr_mean,1), round(qm_obs,3))
-      colnames(Ans) <- c('DatesR', paste0('P_',comid), 'Q')
-    }
-    if(is.null(Qobs)==FALSE & is.null(Precip)==TRUE & is.null(PotEvap)==FALSE){
-      qm_obs        <- Qobs[,2]
-      Ans           <- data.frame(DatesMonths, round(pe_mean,1), round(qm_obs,3))
-      colnames(Ans) <- c('DatesR', paste0('E_',comid), 'Q')
-    }
-    if(is.null(Qobs)==TRUE & is.null(Precip)==FALSE & is.null(PotEvap)==FALSE){
-      Ans           <- data.frame(DatesMonths, round(pr_mean,1), round(pe_mean,1))
-      colnames(Ans) <- c('DatesR', paste0('P_',comid), paste0('E_',comid))
-    }
-    if(is.null(Qobs)==TRUE & is.null(Precip)==FALSE & is.null(PotEvap)==TRUE){
-      Ans           <- data.frame(DatesMonths, round(pr_mean,1))
-      colnames(Ans) <- c('DatesR', paste0('P_',comid))
-    }
-    if(is.null(Qobs)==TRUE & is.null(Precip)==TRUE & is.null(PotEvap)==FALSE){
-      Ans           <- data.frame(DatesMonths, round(pe_mean,1))
-      colnames(Ans) <- c('DatesR', paste0('E_',comid))
-    }
-    if(Save==TRUE){
-      dir.create('./Inputs')
-      write.table(Ans, file='./Inputs/Inputs_model.txt', sep='\t', col.names=TRUE, row.names=FALSE)
-    }
+  if (!is.null(Qobs)) {
+    Flow <- round(Qobs, 3)
+    data_list <- c(data_list, Q = Flow)
+  }
 
-  message('Done!')
+  Ans <- as.data.frame(data_list)
+
+  # ==== Optional: Save to file ====
+  if (Save) {
+    if (!dir.exists("./Inputs")) dir.create("./Inputs")
+    write.table(Ans, file = "./Inputs/Inputs_model.txt", row.names = FALSE)
+  }
+
+  cat("Processing completed successfully.")
   toc()
   return(Ans)
+}
 
-}# End (not run)
